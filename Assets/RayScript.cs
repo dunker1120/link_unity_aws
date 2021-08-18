ca0ae056cdbbb4c8840f858443f4ed575818646f
hiroki-funou <study.programming.nuno@gmail.com>
2021-08-19 06:49:01 +0900

    - add Ray script

---
+++ Assets/RayScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RayScript : MonoBehaviour
+{
+    [SerializeField]
+    float shootRange = 1.2f;
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        //レイを飛ばす
+        if (Physics.Raycast(ray, shootRange))
+        {
+            // TODO S3からテキストを取得し、反映
+        }
+    }
+}
